c93fd05617ea5374136742a1561d6677e5526659
jackinjaxx <jackinjaxx2002@gmail.com>
2022-11-02 01:20:52 -0600

    add timer for every 2 seconds

--- src/App/Controller/ControllerMenu.java
+++ src/App/Controller/ControllerMenu.java
@@ -17,7 +17,29 @@ public class ControllerMenu {
     }
 
     public void initEvents() {
+        eventsTermometro();
+        eventsLabelButtons();
+        viewMenu.labelGitHub.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                try {
+                    java.awt.Desktop.getDesktop().browse(java.net.URI.create("https://github.com/jackinjaxx"));
+                } catch (java.io.IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        });
+        viewMenu.labelTwitter.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                try {
+                    java.awt.Desktop.getDesktop().browse(java.net.URI.create("https://twitter.com/jackinjaxx01"));
+                } catch (java.io.IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        });
+    }
 
+    public void eventsLabelButtons() {
         viewMenu.labelButton1.addMouseListener(new java.awt.event.MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
@@ -36,12 +58,13 @@ public class ControllerMenu {
         });
         viewMenu.labelButton1.addFocusListener(new FocusListener() {
             boolean isFocus = false;
+
             @Override
             public void focusGained(FocusEvent e) {
-                if(isFocus) {
+                if (isFocus) {
                     viewMenu.labelButton1.setIcon(new ImageIcon("Resources/Images/buttonMenuHover.png"));
 
-                }else{
+                } else {
                     isFocus = true;
                 }
             }
@@ -79,24 +102,23 @@ public class ControllerMenu {
                 viewMenu.labelButton2.setIcon(new ImageIcon("Resources/Images/button2MenuActive.png"));
             }
         });
+    }
 
-        viewMenu.labelGitHub.addMouseListener(new MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                try {
-                    java.awt.Desktop.getDesktop().browse(java.net.URI.create("https://github.com/jackinjaxx"));
-                } catch (java.io.IOException e) {
-                    System.out.println(e.getMessage());
-                }
+    public void eventsTermometro(){
+        ImageIcon[] images = new ImageIcon[41];
+        int j = 0;
+        for (int i = -30; i <= 50; i=i+2) {
+            images[j] = new ImageIcon("Resources/Images/termometro/termometro_" + i + ".png");
+            j++;
+        }
+        //for every 2 seconds
+        Timer timer = new Timer(2000, e -> {
+            double temp = Double.parseDouble(viewMenu.jTTemperatura.getText());
+            System.out.println(temp);
+            if(temp==50){
+                viewMenu.labelTermometro.setIcon(images[40]);
             }
         });
-        viewMenu.labelTwitter.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                try {
-                    java.awt.Desktop.getDesktop().browse(java.net.URI.create("https://twitter.com/jackinjaxx01"));
-                } catch (java.io.IOException e) {
-                    System.out.println(e.getMessage());
-                }
-            }
-        });
+        timer.start();
     }
 }
