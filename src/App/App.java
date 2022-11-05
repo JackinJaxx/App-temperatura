f801fc48f46c228c138059d9d344e5eb61da486b
jackinjaxx <jackinjaxx2002@gmail.com>
2022-11-04 23:41:02 -0600

    agregue funcionalidad al metodo eventsTermometro() 2

--- src/App/App.java
+++ src/App/App.java
@@ -8,5 +8,6 @@ public class App {
     public static void main(String[] args) {
         Runnable runApplication = () -> new ControllerMenu();
         SwingUtilities.invokeLater(runApplication);
+        System.out.println("hola guichito");
     }
 }
